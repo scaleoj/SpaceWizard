724bc478b92d90b5bfee9ed863946ef7c8a1d080
Polarschaf <schneidv@hochschule-trier.de>
2019-10-24 15:45:59 +0200

    -Unity Atoms added and other stuff

---
+++ Assets/Scripts/Character/Stats/Stat.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stat : MonoBehaviour
+{
+   [SerializeField] private int currentValue;
+   [SerializeField] private int stdMaxValue;
+}
