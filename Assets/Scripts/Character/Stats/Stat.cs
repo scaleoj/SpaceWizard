d46d64be9ca9990cf9bb2d581436b0ec1ca573a1
Polarschaf <schneidv@hochschule-trier.de>
2019-10-24 16:14:19 +0200

    -Stats basics

--- Assets/Scripts/Character/Stats/Stat.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Stat : MonoBehaviour
-{
-   [SerializeField] private int currentValue;
-   [SerializeField] private int stdMaxValue;
-}
---
+++ Assets/Scripts/Character/Stats/Stats.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityAtoms;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+   [SerializeField] private IntVariable currentValue;
+   [SerializeField] private IntVariable maxValue;
+
+   public IntVariable CurrentValue
+   {
+      get => currentValue;
+      set => CurrentValue.Value = Mathf.Clamp(value.Value, 0, maxValue.Value);
+   }
+}
