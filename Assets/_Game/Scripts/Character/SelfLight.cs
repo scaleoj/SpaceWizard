66a2304491e7920d618868921750517e7fe02717
Polarschaf <schneidv@hochschule-trier.de>
2019-11-06 20:58:42 +0100

    -SelfLight Script

---
+++ Assets/_Game/Scripts/Character/SelfLight.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityAtoms;
+using UnityEngine;
+
+public class SelfLight : MonoBehaviour, IAtomListener<GameObject>
+{
+   [SerializeField] private GameObject light;
+   [SerializeField] private GameObjectEvent currentGameObjectChanged;
+
+   private void Start()
+   {
+      currentGameObjectChanged.RegisterListener(this);
+   }
+
+   public GameObject Light
+   {
+      get => light;
+      set => light = value;
+   }
+
+   public void OnEventRaised(GameObject item)
+   {
+      if (item == gameObject)
+      {
+         if (item.GetComponent<SelfLight>() != null)
+         {
+            item.GetComponent<SelfLight>().Light.SetActive(true);
+         }  
+      }
+      else
+      {
+         Light.SetActive(false);
+      }
+      
+   }
+}
