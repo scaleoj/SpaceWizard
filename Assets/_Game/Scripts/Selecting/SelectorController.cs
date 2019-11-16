7d15c75f59fec09627b0168db7027c22b30ef001
Polarschaf <schneidv@hochschule-trier.de>
2019-11-16 14:00:04 +0100

    - Cleaned up the project - Using new TileGrid in the TestStage - Added Selector script and model for selecting - Visual Updates

---
+++ Assets/_Game/Scripts/Selecting/SelectorController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityAtoms;
+using UnityEngine;
+
+public class SelectorController : MonoBehaviour, IAtomListener<GameObject>
+{
+    
+    [SerializeField] private GameObjectEvent currentGameObjectChanged;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentGameObjectChanged.RegisterListener(this);
+    }
+
+
+    public void OnEventRaised(GameObject item)
+    {
+        if (item.GetComponentInParent<TileGrid>() != null)
+        {
+            gameObject.transform.position = item.transform.position;
+        }
+        else
+        {
+            Debug.Log("Not a Tile.");
+        }
+    }
+}
