34df8960ab5f560958a274e001a894a4a809809e
Polarschaf <schneidv@hochschule-trier.de>
2019-11-21 15:18:48 +0100

    -Preparation for rotationable cam (Input and tries that failed) -Implemented Orthographic cam mode (Lighting is bugged in that mode) -Added MoveSelect Mesh and material -Some Skybox changes

--- Assets/_Game/Scripts/InputHandler/PlayerInputProvider.cs
+++ Assets/_Game/Scripts/InputHandler/PlayerInputProvider.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerInputProvider : MonoBehaviour, IInputprovider
 {
+    [SerializeField] private float deadButtonTime;
+    private bool buttonQwasPressed = false;
+    private bool buttonEwasPressed = false;
+    
     public bool mouse0Down()
     {
         return Input.GetMouseButtonDown(0);
@@ -37,5 +42,45 @@ public class PlayerInputProvider : MonoBehaviour, IInputprovider
     public Vector3 mousePos()
     {
         return Input.mousePosition;
+    }
+
+    public bool RotateLeft()
+    {
+        if (Input.GetAxis("Q") == 0f)
+        {
+            buttonQwasPressed = false;
+        }
+        
+        if (buttonQwasPressed)
+        {
+            return false;
+        }
+        
+        if (Input.GetAxis("Q") == 1f)
+        {
+            buttonQwasPressed = true;
+        }
+
+        return buttonQwasPressed;
+    }
+
+    public bool RotateRight()
+    {
+        if (Input.GetAxis("E") == 0f)
+        {
+            buttonEwasPressed = false;
+        }
+        
+        if (buttonEwasPressed)
+        {
+            return false;
+        }
+        
+        if (Input.GetAxis("E") == 1f)
+        {
+            buttonEwasPressed = true;
+        }
+
+        return buttonEwasPressed;
     }
 }
