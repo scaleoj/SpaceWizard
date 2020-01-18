cd62392848442913e2460dde5728c1d63966c138
Johannes H <johannesherfurth@web.de>
2020-01-18 18:08:28 +0100

    Ai Update

--- Assets/_Game/Scripts/GameFlow/GameFlowControl.cs
+++ Assets/_Game/Scripts/GameFlow/GameFlowControl.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityAtoms;
+﻿using UnityAtoms;
 using UnityEngine;
 using _Game.Scripts.Character.Stats;
 using _Game.Scripts.GameFlow;
