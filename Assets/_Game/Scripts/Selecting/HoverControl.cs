65fd7161b7a591abd6732f5a9b35a5cc058e7823
Polarschaf <schneidv@hochschule-trier.de>
2020-01-21 12:18:17 +0100

    -Hover bugfix

--- Assets/_Game/Scripts/Selecting/HoverControl.cs
+++ Assets/_Game/Scripts/Selecting/HoverControl.cs
@@ -7,8 +7,12 @@ public class HoverControl : MonoBehaviour, IAtomListener<GameObject>
 {
     [SerializeField] private GameObjectEvent hoverGOChanged;
     [SerializeField] private GameObjectVariable clickedGameObject;
+    [SerializeField] private PlayerInputProvider m_input;
+    [SerializeField] private BoolVariable mouseOverUI;
 
     private GameObject lastHovered;
+
+    private bool wasClicked;
 
     private TileContainer.tileState saveTileState;
 
@@ -19,35 +23,38 @@ public class HoverControl : MonoBehaviour, IAtomListener<GameObject>
         lastHovered = null;
     }
 
+    void Update()
+    {
+        if (!mouseOverUI.Value)
+        {
+            if (!wasClicked)
+            {
+                wasClicked = m_input.mouse0Down();
+            }   
+        }
+
+    }
+
 
     public void OnEventRaised(GameObject item)
     {
-       /* if (item.GetComponent<TileContainer>() != null && item.layer == 9 && item.GetComponent<TileContainer>().State != TileContainer.tileState.SELECTED)
-        {
-            if (lastHovered != null && clickedGameObject.Value != lastHovered)
-            {
-                lastHovered.GetComponent<TileContainer>().State = saveTileState;
-            }
-            
-            saveTileState = item.GetComponent<TileContainer>().State;
-            lastHovered = item;
-            item.GetComponent<TileContainer>().State = TileContainer.tileState.HOVERING;
-        }
-        else
-        {
-            lastHovered.GetComponent<TileContainer>().State = TileContainer.tileState.NORMAL;
-        }*/
-       
-       
-
-       if (item != null && item.layer == 9)
+        if (item != null && item.layer == 9)
        {
            if (item.GetComponent<TileContainer>().State != TileContainer.tileState.SELECTED)
            {
                if (lastHovered != null)
                {
-                   lastHovered.GetComponent<TileContainer>().State = saveTileState;
+                   if (wasClicked)
+                   {
+                       lastHovered.GetComponent<TileContainer>().State = TileContainer.tileState.NORMAL;
+                       wasClicked = false;
+                   }
+                   else
+                   {
+                       lastHovered.GetComponent<TileContainer>().State = saveTileState;
+                   }
                }
+
                saveTileState = item.GetComponent<TileContainer>().State;
                lastHovered = item;
                item.GetComponent<TileContainer>().State = TileContainer.tileState.HOVERING;
