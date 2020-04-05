11530b1e65cb8ab2ed5c7924d3fde03111026e1a
Polarschaf <schneidv@hochschule-trier.de>
2020-04-05 13:43:43 +0200

    - I LIKE TRAINS - ToolTip offset now scales properly with screen size

--- Assets/_Game/Scripts/Ambient/Train.cs
+++ Assets/_Game/Scripts/Ambient/Train.cs
@@ -3,28 +3,43 @@ using System.Collections.Generic;
 using UnityAtoms;
 using UnityEngine;
 
-public class Car : MonoBehaviour
+public class Train : MonoBehaviour
 {
     [SerializeField] private Vector3 startPosition;
     [SerializeField] private Vector3 endPosition;
     [SerializeField] private float lerpSpeedMultiplier;
     [Range(0f,1f)][SerializeField] private float frequency;
     [SerializeField] private float minDeadTime;
-    [SerializeField] private BoolVariable trainIsMoving;
-    private bool isMoving;
+    [SerializeField] private BoolVariable isMoving;
+    [Tooltip("Delay in seconds after it switched isMoving to true.")][SerializeField] private float startDelay;
+    private bool waitForDelay = false;
     private float currTime;
     
     // Update is called once per frame
     void Update()
     {
-        if (isMoving)
+        if (waitForDelay)
+        {
+            currTime += Time.deltaTime;
+            if (currTime >= startDelay)
+            {
+                waitForDelay = false;
+                currTime = 0f;
+            }
+            else
+            {
+                return;
+            }
+        }
+        
+        if (isMoving.Value)
         {
             currTime += Time.deltaTime * lerpSpeedMultiplier;
             transform.position = Vector3.Lerp(startPosition, endPosition, currTime);
             if (currTime >= 1f)
             {
                 transform.position = startPosition;
-                isMoving = false;
+                isMoving.Value = false;
                 currTime = 0f;
             }
         }
@@ -35,9 +50,10 @@ public class Car : MonoBehaviour
             {
                 float randNum = UnityEngine.Random.Range(0f,1f);
         
-                if (randNum <= frequency && !trainIsMoving.Value)
+                if (randNum <= frequency)
                 {
-                    isMoving = true;
+                    isMoving.Value = true;
+                    waitForDelay = true;
                     currTime = 0f;
                 } 
             }
