e0d4146e30729cfdc9d6395ba096d39fbce082e1
Polarschaf <schneidv@hochschule-trier.de>
2020-04-13 11:23:05 +0200

    - Sliders now show the exact values as text - Fixed DMG and HIT/MISS indicator being inside the sliders - Changed UIElement when hovering over the enemy

--- Assets/_Game/Scripts/UI/Slider/CharSliderControl.cs
+++ Assets/_Game/Scripts/UI/Slider/CharSliderControl.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using _Game.Scripts.Character.Stats;
 using _Game.Scripts.GameFlow;
+using TMPro;
 using UnityAtoms;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,9 +12,11 @@ using Void = UnityAtoms.Void;
 public class CharSliderControl : MonoBehaviour, IAtomListener<Void>
 {
     [SerializeField] private VoidEvent updateHUD;
+    [SerializeField] private TextMeshProUGUI textMesh;
     [Tooltip("Normal = HP, Magic = MagicShield, Physical = Armor")][SerializeField] private CharacterStat.DamageType _type;
     private CharacterStat _characterStat;
     private Slider _slider;
+    
     void Start()
     {
         updateHUD.RegisterListener(this);
@@ -29,14 +32,17 @@ public class CharSliderControl : MonoBehaviour, IAtomListener<Void>
                case CharacterStat.DamageType.Magic:
                    _slider.maxValue = _characterStat.MaxMs;
                    _slider.value = _characterStat.CurrentMs;
+                   textMesh.text = "MS: " + _characterStat.CurrentMs  + "/"  + _characterStat.MaxMs;
                    break;
                case CharacterStat.DamageType.Normal:
                    _slider.maxValue = _characterStat.MaxHealth;
                    _slider.value = _characterStat.CurrentHealth;
+                   textMesh.text = "HP: \n" + _characterStat.CurrentHealth + "/"  + _characterStat.MaxHealth;
                    break;
                case CharacterStat.DamageType.Physical:
                    _slider.maxValue = _characterStat.MaxArmor;
                    _slider.value = _characterStat.CurrentArmor;
+                   textMesh.text = "PS: " + _characterStat.CurrentArmor + "/"  + _characterStat.MaxArmor;
                    break;
         }
     }
