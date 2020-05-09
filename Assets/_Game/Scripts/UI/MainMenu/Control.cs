fc2b36c73d8b65b2b0c06d64499c895315ad3db9
Johannes H <johannesherfurth@web.de>
2020-05-09 12:59:41 +0200

    lightmap

--- Assets/_Game/Scripts/UI/MainMenu/Control.cs
+++ Assets/_Game/Scripts/UI/MainMenu/Control.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using TMPro.Examples;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Control : MonoBehaviour
